d4039bc00c31f39125c25b92c331bf0e1604609f
zainalrfn <za036335@gmail.com>
2023-03-28 21:49:16 -0700

    Menambahkan File

---
+++ gabungvariabel.js
@@ -0,0 +1,14 @@
+var lastName;
+lastName = "ZainalArifin";
+console.log(lastName);
+
+let Name;
+Name = "Roti";
+console.log(lastName);
+
+const z = 10;
+console.log(z);
+document.write(lastName+"</br>");
+document.write(Name+"</br>");
+document.write(z+"</br>");
+
